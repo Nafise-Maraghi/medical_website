d6994c68564167e5799bbb470b52cb4aa363aef4
Precious <nafiseh.maraghi@gmail.com>
2023-03-03 16:09:08 +0330

    sending email added

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os.path
 from pathlib import Path
 
+from mail_password import sender_mail, mail_password
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +127,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Sending mail settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = sender_mail
+EMAIL_HOST_PASSWORD = mail_password
--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import render
 
@@ -10,6 +11,12 @@ def index(request):
         email = request.POST["email"]
         symptoms = request.POST["symptoms"]
         date = request.POST["date"]
+
+        subject = "Medical website - Message received."
+        message = f"Dear {patient}\nwe have received your message and will get back to you soon!"
+        sender = settings.EMAIL_HOST_USER
+        recipients = [email]
+        send_mail(subject, message, sender, recipients)
 
         return render(request, 'index.html', {"patient": patient})
 
